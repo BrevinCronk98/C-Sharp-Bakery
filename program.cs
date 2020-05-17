b58985d52b5ea4a83c913e7a79af74a51a6c1b50
brevin <cronkbrevin98@gmail.com>
2020-05-17 15:08:20 -0700

    work on resubmit

--- program.cs
+++ program.cs
@@ -11,8 +11,43 @@ namespace Order
         {
             List<BreadOrder> Breads = new List<BreadOrder>(){};
             BreadOrder myBread = new BreadOrder();
-            myBread.Game();
+            Console.WriteLine("Here is our Esteemed Bread Selection!: " + _breadOne + " is " + _costOne + " Dollars, " + _breadTwo + " is " + _costTwo + " Dollars, " + _breadThree + " is " +  _costThree + " Dollars.." );
+            int num = 1;
+            
+            for(int i = 0; i < num; i ++)
+            {
+                Console.WriteLine("What Type of Bread Would You Like? To Make it Simple for you Guys, Press 1 for White Bread, Press 2 for French Bread, or Press 3 for Elven Bread, oooo");
+                string choice = Console.ReadLine();
+                if(choice == "1")
+                {
+                    _breadType = _breadOne;
+                    Console.WriteLine("How Many Slices of " + _breadType +" Would You Like");
+                     _countOne = int.Parse(Console.ReadLine());
+                   
+                } else if(choice == "2")
+                {
+                    _breadType = _breadTwo;
+                    Console.WriteLine("How Many Slices of " + _breadType +" Would You Like");
+                    _countTwo = int.Parse(Console.ReadLine());
+                   
+                } else if( choice == "3")
+                {
+                    _breadType = _breadThree;
+                    Console.WriteLine("How Many Slices of " + _breadType +" Would You Like");
+                    _countThree = int.Parse(Console.ReadLine());
+                   
+                } else
+                {
+                    Console.WriteLine("We Dont Have That Bread Thanks for Checking for Errors");
+                }
+
+                
+                Console.WriteLine("Do You Want to Order More? Press Y for Yes, and N for No");
+                string yes = Console.ReadLine();
+            }
             Breads.Add(myBread);
+            
+            
             List<PastryOrder> Pastries = new List<PastryOrder>(){};
             PastryOrder myPastry = new PastryOrder();
             myPastry.Game();
